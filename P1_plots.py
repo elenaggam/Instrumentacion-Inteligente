05ea0f6fa06b57e17252b51f9595a4e187906552
Hernan Gracia <hernangracia2003@gmail.com>
2025-11-08 14:18:26 +0100

    Más cambios

--- P1_plots.py
+++ P1_plots.py
@@ -4,22 +4,46 @@ import Analysis as A
 import Plotting as P
 
 
+# Medidas caja (grueso)
+freq, vi, magn, phase = np.loadtxt('Bode/1_Caja/DEF_flog100-10000000.0-10000000.0_steps30_avg8_Vin1.txt', unpack=True)
 
-# Medidas caja
+# P.bode_magnitude(freq, magn, show = True, directory='Bode/1_Caja/DEF1_')
+# P.bode_phase(freq, phase, deltaTicks=90, show = True, directory='Bode/1_Caja/DEF1_')
+P.bode_diagrams(freq, magn, phase, directory='Bode/1_Caja/Bode_CG_All')
+
+input("Enter para continuar.")
+plt.close('all')
+
+
+# Medidas caja (fino)
+freq, vi, magn, phase = np.loadtxt('Bode/1_Caja/DEF_flog1000-40000-500000.0_steps70_avg8_Vin1.txt', unpack=True)
+
+# P.bode_magnitude(freq, magn, show = True, directory='Bode/1_Caja/DEF2_')
+# P.bode_phase(freq, phase, show = True, directory='Bode/1_Caja/DEF2_')
+P.bode_diagrams(freq, magn, phase, directory='Bode/1_Caja/Bode_CF_All', infLim=-44)
+
+input("Enter para continuar.")
+plt.close('all')
+
+
+# Medidas caja (fino + medidas)
 freq, vi, magn, phase = np.loadtxt('Bode/1_Caja/DEF_flog1000-40000-500000.0_steps70_avg8_Vin1.txt', unpack=True)
 
 f_low, max_magn = A.bandwidth_gain(freq, magn)
 f_cut, min_magn = A.stopband_attenuation(freq, magn)
-
 delta = 20*np.log10(max_magn/min_magn)
 print("\nGain:\t\tA_0 = {max_magn}\nf passband:\tf_c = {f_low:.0f}\nf stopband:\tf_s = {f_cut:.0f}\nsb attenuation: A_sb = {delta:.2f} dB\n".format(max_magn=max_magn, f_low=f_low, f_cut=f_cut, delta=delta))
-P.bode_plots(freq, magn, phase, show = False, f_low=f_low, max_magn=max_magn, f_cut=f_cut, min_magn=min_magn)
 
+# P.bode_magnitude(freq, magn, show = True, directory='Bode/1_Caja/DEF3_', f_low=f_low, max_magn=max_magn, f_cut=f_cut, min_magn=min_magn)
+# P.bode_phase(freq, phase, show = True, directory='Bode/1_Caja/DEF3_', f_low=f_low, f_cut=f_cut)
+P.bode_diagrams(freq, magn, phase, directory='Bode/1_Caja/Bode_CFM_All', infLim=-44, f_low=f_low, max_magn=max_magn, f_cut=f_cut, min_magn=min_magn)
+
+input("Enter para continuar.")
+plt.close('all')
 
 
 # Datos Tina
-freq, magn, phase = np.loadtxt(
-    'Bode/2_Tina/DatosBodeTinaFINO.txt',
+freq, magn, phase = np.loadtxt('Bode/2_Tina/DatosBodeTinaFINO.txt',
     unpack=True,
     skiprows=1,
     delimiter=None,  # acepta tab o espacios
@@ -31,9 +55,61 @@ freq, magn, phase = np.loadtxt(
 )
 magn = 10**(magn/20)  # dB a V/V
 
-f_low, max_magn = A.bandwidth_gain(freq, magn)
-f_cut, min_magn = A.stopband_attenuation(freq, magn)
+# f_low, max_magn = A.bandwidth_gain(freq, magn)
+# f_cut, min_magn = A.stopband_attenuation(freq, magn)
+# delta = 20*np.log10(max_magn/min_magn)
+# print("\nGain:\t\tA_0 = {max_magn}\nf passband:\tf_c = {f_low:.0f}\nf stopband:\tf_s = {f_cut:.0f}\nsb attenuation: A_sb = {delta:.2f} dB\n".format(max_magn=max_magn, f_low=f_low, f_cut=f_cut, delta=delta))
 
-delta = 20*np.log10(max_magn/min_magn)
-print("\nGain:\t\tA_0 = {max_magn}\nf passband:\tf_c = {f_low:.0f}\nf stopband:\tf_s = {f_cut:.0f}\nsb attenuation: A_sb = {delta:.2f} dB\n".format(max_magn=max_magn, f_low=f_low, f_cut=f_cut, delta=delta))
-P.bode_plots(freq, magn, phase, show=True, f_low=f_low, max_magn=max_magn, f_cut=f_cut, min_magn=min_magn)
+# P.bode_magnitude(freq, magn, show = True, directory='Bode/2_Tina/')
+# P.bode_phase(freq, phase, show = True, directory='Bode/2_Tina/')
+P.bode_diagrams(freq, magn, phase, directory='Bode/2_Tina/Bode_Tina_All', infLim=-44)
+
+input("Enter para continuar.")
+plt.close('all')
+
+
+# Medidas montaje
+freq, vi, magn, phase = np.loadtxt('Bode/3_Montaje/flog1000-1000000.0-1000000.0_steps55_avg8_Vin1.txt', unpack=True)
+
+# f_low, max_magn = A.bandwidth_gain(freq, magn)
+# f_cut, min_magn = A.stopband_attenuation(freq, magn)
+# delta = 20*np.log10(max_magn/min_magn)
+# print("\nGain:\t\tA_0 = {max_magn}\nf passband:\tf_c = {f_low:.0f}\nf stopband:\tf_s = {f_cut:.0f}\nsb attenuation: A_sb = {delta:.2f} dB\n".format(max_magn=max_magn, f_low=f_low, f_cut=f_cut, delta=delta))
+
+# P.bode_magnitude(freq, magn, show = True, directory='Bode/3_Montaje/')
+# P.bode_phase(freq, phase, show = True, directory='Bode/3_Montaje/')
+P.bode_diagrams(freq, magn, phase, directory='Bode/3_Montaje/Bode_EXP_All')
+
+input("Enter para continuar.")
+plt.close('all')
+
+
+# TINA vs CAJA
+freq, vi, magn, phase = np.loadtxt('Bode/1_Caja/DEF_flog1000-40000-500000.0_steps70_avg8_Vin1.txt', unpack=True)
+freq2, magn2, phase2 = np.loadtxt('Bode/2_Tina/DatosBodeTinaFINO.txt',
+    unpack=True,
+    skiprows=1,
+    delimiter=None,  # acepta tab o espacios
+    converters={
+        0: lambda s: float(s.decode().replace(',', '.') if isinstance(s, bytes) else s.replace(',', '.')),
+        1: lambda s: float(s.decode().replace(',', '.') if isinstance(s, bytes) else s.replace(',', '.')),
+        2: lambda s: float(s.decode().replace(',', '.') if isinstance(s, bytes) else s.replace(',', '.')),
+    },
+)
+magn2 = 10**(magn2/20)  # dB a V/V
+
+P.bode_diagrams(freq, magn, phase, label1='Medida Caja', label2='Simulación TINA', freq2=freq2, magn2=magn2, phase2=phase2, directory='Bode/2_Tina/Tina_vs_Caja_All')
+
+input("Enter para continuar.")
+plt.close('all')
+
+
+# EXP vs CAJA
+freq, vi, magn, phase = np.loadtxt('Bode/1_Caja/DEF_flog1000-40000-500000.0_steps70_avg8_Vin1.txt', unpack=True)
+freq2, vi2, magn2, phase2 = np.loadtxt('Bode/3_Montaje/flog1000-1000000.0-1000000.0_steps55_avg8_Vin1.txt', unpack=True)
+
+
+P.bode_diagrams(freq, magn, phase, label1='Medida Caja', label2='Medida Montaje', freq2=freq2, magn2=magn2, phase2=phase2, directory='Bode/3_Montaje/Exp_vs_Caja_All')
+
+input("Enter para continuar.")
+plt.close('all')
--- Plotting.py
+++ Plotting.py
@@ -34,47 +34,139 @@ def bode_magnitude(freq, magn, directory=None, show=True, f_low=None, max_magn=N
     # plt.legend()
     plt.xscale('log')
 
-    x=np.log(np.logspace(np.log10(min(freq)), np.log10(1e4), 100))
-    #plt.plot(x, 20*x)
+    plt.tight_layout()
 
     if directory is not None:
         plt.savefig(directory+'Bode_Magnitud.png')
     if show:
-        plt.show()
+        plt.show(block=False)
 
     return
 
-def bode_phase(freq, phase, directory=None, show=True, f_low=None, f_cut=None):
+def bode_phase(freq, phase, deltaTicks=45, directory=None, show=True, f_low=None, f_cut=None):
 
+    phase = np.asarray(phase, dtype=float)
+    phase = np.where(phase > 180, phase - 360, phase)
+    phase = np.where(phase < -180, phase + 360, phase)
     plt.figure()
     # plt.title('Diagrama de Bode - Fase')
     plt.xlabel('Frecuencia (Hz)')
     plt.ylabel('Fase ($^\\circ$)')
+    plt.ylim(top=180)
     ax = plt.gca()
     ax.xaxis.label.set_size(12)
     ax.yaxis.label.set_size(12)
     plt.tick_params(axis='both', which='major', labelsize=12)
+    ymin = np.min(phase)
+    ymax = np.max(phase)
+    yt_min = np.floor(ymin / deltaTicks) * deltaTicks
+    yt_max = np.ceil(ymax / deltaTicks) * deltaTicks
+    yticks = np.arange(yt_min, yt_max + 1e-6, deltaTicks)
+    ax.set_yticks(yticks)
     plt.grid(zorder=-1)
     plt.plot(freq, phase, marker='o', linestyle='-')
     plt.xscale('log')
     if f_low is not None:
         plt.axvline(f_low, color='red', linestyle='--', label=f'fc={f_low:.2f} Hz')
-    # if f_cut is not None:
-    #     plt.axvline(f_cut, color='black', linestyle='--', label=f'fc={f_cut:.2f} Hz')
+    if f_cut is not None:
+        plt.axvline(f_cut, color='black', linestyle='--', label=f'fc={f_cut:.2f} Hz')
     # plt.legend()
 
+    plt.tight_layout()
+    
     if directory is not None:
         plt.savefig(directory+'Bode_Fase.png')
     if show:
-        plt.show()
-
-    return
-
-def bode_plots(freq, magn, phase, directory=None, show=True, f_low=None, max_magn=None, f_cut=None, min_magn=None):
-    bode_magnitude(freq, magn, directory, show=False, f_low=f_low, max_magn=max_magn, f_cut=f_cut, min_magn=min_magn)
-    bode_phase(freq, phase, directory, show, f_low=f_low, f_cut=f_cut)
+        plt.show(block=False)
 
     return
 
 
 #añadir pendientes, bw, etc
+
+
+
+def bode_diagrams(freq, magn, phase, freq2=None,magn2=None, phase2=None, label1=None, label2=None, directory=None, show=True, deltaTicks=90, infLim=None, f_low=None, max_magn=None, f_cut=None, min_magn=None):
+    
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 5))
+
+    plt.sca(ax1)
+
+    ax = plt.gca()
+    ax.yaxis.label.set_size(12)
+    plt.ylabel('Magnitud (dB)', labelpad=10)
+    plt.tick_params(axis='both', which='major', labelsize=12)
+    plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+    plt.xscale('log')
+    plt.grid(zorder=-1)
+    
+    log_magn=20*np.log10(magn)
+    plt.plot(freq, log_magn, marker='o', linestyle='-')
+    if magn2 is not None:
+        log_magn2=20*np.log10(magn2)
+        plt.plot(freq2, log_magn2, marker='o', markersize=3)
+
+    if f_low is not None:
+        plt.axvline(f_low, color='red', linestyle='--', label=f'fc={f_low:.2f} Hz')
+    if max_magn is not None:
+        log_max_magn=20*np.log10(max_magn)
+        plt.axhline(log_max_magn - 3, color='red', linestyle='--', label=f'{log_max_magn - 3:.2f} dB')
+    if f_cut is not None:
+        plt.axvline(f_cut, color='black', linestyle='--', label=f'fc={f_cut:.2f} Hz')
+    if min_magn is not None:
+        log_min_magn=20*np.log10(min_magn)
+        plt.axhline(log_min_magn, color='black', linestyle='--', label=f'{min_magn:.2f} dB')
+
+    plt.tight_layout()
+
+    if show:
+        plt.show(block=False)
+
+
+    plt.sca(ax2)
+
+    ax = plt.gca()
+    ax.xaxis.label.set_size(12)
+    ax.yaxis.label.set_size(12)
+    plt.xlabel('Frecuencia (Hz)')
+    plt.ylabel('Fase ($^\\circ$)')
+    plt.tick_params(axis='both', which='major', labelsize=12)
+    plt.xscale('log')
+    plt.grid(zorder=-1)
+    ymin = np.min(phase)
+    ymax = np.max(phase)
+    yt_min = np.floor(ymin / deltaTicks) * deltaTicks
+    yt_max = np.ceil(ymax / deltaTicks) * deltaTicks
+    yticks = np.arange(yt_min, yt_max + 1e-6, deltaTicks)
+    ax.set_yticks(yticks)
+    plt.ylim(top=180)
+    if infLim is not None:
+        plt.ylim(bottom=infLim)
+    
+    phase = np.asarray(phase, dtype=float)
+    phase = np.where(phase > 180, phase - 360, phase)
+    phase = np.where(phase < -180, phase + 360, phase)
+    plt.plot(freq, phase, marker='o', linestyle='-')
+    if phase2 is not None:
+        phase2 = np.asarray(phase2, dtype=float)
+        phase2 = np.where(phase2 > 180, phase2 - 360, phase2)
+        phase2 = np.where(phase2 < -180, phase2 + 360, phase2)
+        plt.plot(freq2, phase2, marker='o', markersize=3)
+
+    if f_low is not None:
+        plt.axvline(f_low, color='red', linestyle='--', label=f'fc={f_low:.2f} Hz')
+    if f_cut is not None:
+        plt.axvline(f_cut, color='black', linestyle='--', label=f'fc={f_cut:.2f} Hz')
+
+    if label1 is not None and label2 is not None:
+        plt.legend([label1, label2], fontsize=12)
+
+    plt.tight_layout()
+    
+    if show:
+        plt.show(block=False)
+
+    if directory is not None:
+        plt.savefig(directory+'.png')
+
+    return

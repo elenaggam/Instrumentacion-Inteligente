c85547bf09ee5db9e64b225330cce42030c06e62
Elena <839557@unizar.es>
2025-12-10 13:12:19 +0100

    MEAS RI1 R3

--- P2_good.py
+++ P2_good.py
@@ -15,7 +15,7 @@ def set_pot(pot, value):
     resp = arduino.readline().decode().strip()
     print("Arduino:", resp)
 
-R1 = [255-i for i in range(256)]
+R1 = [255-i for i in range(25)]
 
 
 arduino=serial.Serial('COM6', 9600) #change COM port accordingly
@@ -41,7 +41,7 @@ else:
     instrumento.write('acq:type normal')
 
 # Archivo de datos
-file=open(f'Wave_gen/avg{avg}_Vcc{Vcc}.txt', 'w')
+file=open(f'Waaa', 'w')
 
 
 
@@ -52,20 +52,42 @@ instrumento.write('autoscale')
 
 
 # barrido en R1 (int) -> barrido en frecuencia
+
+file.write("R1\t\tFreq_tr(Hz)\t\tV_tr(V)\t\tDC_tr(V)\t\tFreq_sq(Hz)\t\tV_sq(V)\t\tDC_sq(V)\t\tPhase(deg)\tt\trise_tr(s)\t\tfall_tr(s)\t\trise_sq(s)\t\tfall_sq(s)\n")
+
+def mis_medidas():
+    V_tr=float(instrumento.query('meas:vpp? chan1'))
+    V_sq=float(instrumento.query('meas:vpp? chan2'))
+    freq_tr=float(instrumento.query('meas:freq? chan1'))
+    freq_sq=float(instrumento.query('meas:freq? chan2'))
+    DC_tr=float(instrumento.query('meas:vav? chan1'))
+    DC_sq=float(instrumento.query('meas:vav? chan2'))
+    phas=float(instrumento.query('meas:phase? chan1,chan2'))
+    t_up_tr=float(instrumento.query('meas:ris? chan1'))
+    t_down_tr=float(instrumento.query('meas:fall? chan1'))
+    t_up_sq=float(instrumento.query('meas:ris? chan2'))
+    t_down_sq=float(instrumento.query('meas:fall? chan2'))
+    
+    return V_tr, V_sq, freq_tr, freq_sq, DC_tr, DC_sq, phas, t_up_tr, t_down_tr, t_up_sq, t_down_sq
+
+
+set_pot(0, 255)
+time.sleep(0.1)  # esperar a que el pot se estabilice
+V_tr, V_sq, freq_tr, freq_sq, DC_tr, DC_sq, phas, t_up_tr, t_down_tr, t_up_sq, t_down_sq = mis_medidas()
+instrumento.write(f'chan2:range {V_sq*1.5}V')  
+instrumento.write(f'chan1:range {V_tr*1.5}V')
+instrumento.write(f'chan1:offset {DC_tr}')
+instrumento.write(f'chan2:offset {DC_sq}')
+
 for i in R1:
+    instrumento.write(f'tim:range {5/freq_tr}')
     set_pot(0, i)
     time.sleep(0.1)  # esperar a que el pot se estabilice
+    V_tr, V_sq, freq_tr, freq_sq, DC_tr, DC_sq, phas, t_up_tr, t_down_tr, t_up_sq, t_down_sq = mis_medidas() 
+    file.write(f"{i}\t\t{freq_tr:.2f}\t\t{V_tr:.4f}\t\t{DC_tr:.4f}\t\t{freq_sq:.2f}\t\t{V_sq:.4f}\t\t{DC_sq:.4f}\t\t{phas:.2f}\t\t{t_up_tr:.6f}\t\t{t_down_tr:.6f}\t\t{t_up_sq:.6f}\t\t{t_down_sq:.6f}\n")
 
-    # V_tr=float(instrumento.query('meas:vpp? chan1'))
-    # V_sq=float(instrumento.query('meas:vpp? chan2'))
-    # freq_tr=float(instrumento.query('meas:freq? chan1'))
-    # freq_sq=float(instrumento.query('meas:freq? chan2'))
-    # DC_tr=float(instrumento.query('meas:volt:dc? chan1'))
-    # DC_sq=float(instrumento.query('meas:volt:dc? chan2'))
-    # phas=float(instrumento.query('meas:phase? chan1,chan2'))
 
-    # file.write(f"{i}\t{freq_tr:.2f}\t{V_tr:.4f}\t{DC_tr:.4f}\t{freq_sq:.2f}\t{V_sq:.4f}\t{DC_sq:.4f}\t{phas:.2f}\n")
-    # print(f"R1={i*50/255}, freq_tr={freq_tr:.2f} Hz, Vtr={V_tr:.4f} V, freq_sq={freq_sq:.2f} Hz, Vsq={V_sq:.4f} V")
+
 
 instrumento.close()
 file.close()
--- P2_plots.py
+++ P2_plots.py
@@ -4,11 +4,38 @@ import Analysis as A
 import Plotting as P
 
 
-# Medidas caja (grueso)
-freq, vi, vo, phase = np.loadtxt('try_flog100-10000-600000_steps80_avg8_Vin1.txt', unpack=True)
-magn = vo/vi
+intR, V_tr, V_sq, freq_tr, freq_sq, DC_tr, DC_sq, phas, t_up_tr, t_down_tr, t_up_sq, t_down_sq = np.loadtxt('Wave_gen/R1_R3/new_avg8_Vcc10.txt', unpack=True, skiprows=1)
 
-P.bode_diagrams(freq, magn, phase, directory='final_try', show=True)
+
+R1 = intR*42/255  # Valor real de R1 en kOhm
+
+plt.plot(R1, V_tr, 'o-', label='V_tr')
+plt.legend()
+plt.xlabel('R1 (kOhm)')
+plt.ylabel('Voltage (V)')
+plt.show()
+plt.plot(R1, V_sq, 's-', label='V_sq')
+plt.legend()
+plt.xlabel('R1 (kOhm)')
+plt.ylabel('Voltage (V)')
+plt.show()
+plt.plot(R1, freq_tr, 'o--', label='Freq_tr')
+plt.xlabel('R1 (kOhm)')
+plt.ylabel('Frequency (Hz)')
+plt.legend()
+plt.show()
+plt.plot(R1, freq_sq, 's--', label='Freq_sq')
+plt.xlabel('R1 (kOhm)')
+plt.ylabel('Frequency (Hz)')
+plt.legend()
+plt.show()
+plt.plot(R1, phas, 'd-', label='Phase')
+plt.legend()
+plt.xlabel('R1 (kOhm)')
+plt.ylabel('Phase (deg)')
+plt.show()
+
+
 
 input("Enter para continuar.")
 plt.close('all')
